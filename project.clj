98b18b62f8c6d8160fb3907858fa58cfc84ddec7
Burin Choomnuan <agilecreativity@gmail.com>
2017-02-27 18:00:34 -0500

    Add crypto-random to generate the hex string

--- project.clj
+++ project.clj
@@ -3,14 +3,15 @@
   :url "https://github.com/agilecreativity/encrypted-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[lein-bin "0.3.4"]]}
+  :profiles {:dev {:dependencies [[lein-bin "0.3.5"]]}
              :uberjar {:aot :all}}
   :source-paths ["src/main/clj"]
   :java-source-paths ["src/main/java"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [easy-config "0.1.2"]
-                 [buddy "1.2.0"]
+                 [crypto-random "1.2.0"]
+                 [buddy "1.3.0"]
                  [org.bouncycastle/bcprov-jdk15on "1.56"]
                  [me.raynes/fs "1.4.6"]]
   :bin {:name "encrypted-config"
