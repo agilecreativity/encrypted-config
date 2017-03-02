e3d87f2162959038263084c9fd9dee39cd797568
Burin Choomnuan <agilecreativity@gmail.com>
2017-03-01 23:57:36 -0500

    Adjust the apis and make everything else private

--- project.clj
+++ project.clj
@@ -1,24 +1,14 @@
-(defproject encrypted-config "0.1.0-SNAPSHOT"
-  :description "Encrypted configuration"
+(defproject encrypted-config "0.1.0"
+  :description "Encryption/decryption library for store sensitive configuration."
   :url "https://github.com/agilecreativity/encrypted-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[lein-bin "0.3.5"]]}
-             :uberjar {:aot :all}}
   :source-paths ["src/main/clj"]
   :test-paths ["src/test/clj"]
   :java-source-paths ["src/main/java"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.cli "0.3.5"]
-                 [easy-config "0.1.2"]
                  [crypto-random "1.2.0"]
                  [buddy "1.3.0"]
-                 [org.bouncycastle/bcprov-jdk15on "1.56"]
-                 [me.raynes/fs "1.4.6"]]
-  :bin {:name "encrypted-config"
-        :bin-path "~/bin"
-        :bootclasspath true}
-  :plugins [[lein-bin "0.3.4"]
-            [lein-cljfmt "0.5.3"]
-            [lein-auto "0.1.3"]]
-  :main encrypted-config.core)
+                 [org.bouncycastle/bcprov-jdk15on "1.56"]]
+  :plugins [[lein-cljfmt "0.5.3"]
+            [lein-auto "0.1.3"]])
--- src/test/clj/encrypted_config/core_test.clj
+++ src/test/clj/encrypted_config/core_test.clj
@@ -1,25 +1,18 @@
 (ns encrypted-config.core-test
   (:require [clojure.test :refer :all]
-            [encrypted-config.core :refer :all]
-            [encrypted-config.option :refer :all]))
+            [encrypted-config.core :refer [str->encrypted
+                                           str->decrypted] :as ecc]))
 
 ;; See: https://github.com/clojure-emacs/cider/blob/master/doc/running_tests.md
 ;; To run the test C-c C-t n
 
-(def sample-config
-  (let [sample-config
-        {:url "https://github.com/login"
-         :expected-title "github"
-         :u-type :id
-         :u-expr "login_field"
-         :username "agilecreativity"
-         :p-type :id
-         :p-expr "password"
-         :password "some-password"
-         :browser :chrome
-         }]
-    sample-config))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest encryption->decryption
+  (testing "Simple decryption"
+    (let [input-text "MyP@ssword!"
+          secret-key "MySec$tKey@"
+          [random-hex encrypted-text] (ecc/str->encrypted input-text secret-key)
+          ;; Now let's decrypt the text we have just encrypted
+          decrypted-text (ecc/str->decrypted encrypted-text
+                                             secret-key
+                                             random-hex)]
+      (is (= input-text decrypted-text)))))
